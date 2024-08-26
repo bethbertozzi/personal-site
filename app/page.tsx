2d099ebe0719d307340c1f7790e9920c1d072bd8
Beth Bertozzi <elizabeth.bertozzi@mail.utoronto.ca>
2024-08-26 17:48:44 -0400

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <section className="mt-5 lg:mt-16">
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Beth Bertozzi
+      </h1>
+      <p className="mb-4">
+        {`My resume will go here`}
+      </p>
+    </section>
+  )
+}
